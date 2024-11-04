ce7f0d620153d6be3f06fba11e4e24a4024be376
MartinMaybe <martingonzalez@u.boisestate.edu>
2024-11-03 23:20:29 -0700

    Project4 Complete

--- Retrospective.md
+++ Retrospective.md
@@ -1,16 +1,21 @@
 # Retrospective
 
-- name: Jane Doe
-- email: janedoe@u.boisestate.edu
+- name: Martin Gonzalez
+- email: martingonzalez@u.boisestate.edu
 
 ## Experience
 
-TODO
+I have completed Project 4. This project was pretty simple at the start, as styling and organizing is pretty easy and fun. The database stuff was just unfamiliar and I struggled to understand it. I don’t even know if I did it right or if it’s working properly. I began with adjusting the pug file. I did it all in html and then used the linked website to convert it. It became easier to understand as I saw it being converted. It’s very similar with small changes like no closing tags and different syntax. It makes me wonder why it is better or why use it as an alternative for the most part, but I can also recognize it’s uses. 
+
+I made a barebones template of the pug and took to the CSS to make my app look a lot prettier. I changed the headers and texts to look more appealing. Then, I added spacings and padding to the texts and elements. It was a mixture of changing position adjusting values to make not look horrid. Then, I changed the colors and messed around with them. I used a website that gave me gradients for certain shades and it was fun to mess with. 
+
+The database is where I ran into problems. I simply copied the example code we were given for the most part. I did minor adjustments to make sure they worked together, but I don’t see changes or know if it worked properly. I take it as a win as my program didn’t crash and die on me. It’s new to me so I definitely struggled to understand it. 
+
 
 ## Known issues or Bugs
 
-TODO
+No known issues or bugs
 
 ## Sources used
 
-TODO
+Zybook, https://cssgradient.io for colors, 
--- src/server.js
+++ src/server.js
@@ -2,12 +2,9 @@ import express from 'express'
 import sql from 'sqlite3'
 
 const sqlite3 = sql.verbose()
-
-// Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
-// This is just for testing you would not want to create the table every
-// time you start up the app feel free to improve this code :)
+// Create an in memory table to use
 db.run(`CREATE TABLE todo (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     task TEXT NOT NULL)`)
@@ -19,23 +16,51 @@ app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
-    //TODO You will need to do a SQL select here
-    //TODO You will need to update the code below!
+    //You will need to do a SQL select here
+    //You will need to update the code below!
     console.log('GET called')
-    res.render('index')
+    const local = { tasks: [] };
+    db.each('SELECT id, task FROM todo', function (err, row) {
+        if (err) {
+            console.log(err);
+        } else {
+            local.tasks.push({ id: row.id, task: row.task });
+        }
+    }, function (err, numrows) {
+        if (!err) {
+            res.render('index', local);
+        } else {
+            console.log(err);
+        }
+    })
 
 })
 
 app.post('/', function (req, res) {
-    console.log('adding todo item')
-    //TODO You will need to to do a SQL Insert here
+    console.log('adding todo item');
+    
+    const stmt = db.prepare('INSERT INTO todo (task) VALUES (?)');
+    stmt.run(req.body.todo, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        stmt.finalize();
+        res.redirect('/'); 
+    })
 
 })
 
 app.post('/delete', function (req, res) {
-    console.log('deleting todo item')
-    //TODO you will need to delete here
-
+    console.log('deleting todo item');
+    
+    const stmt = db.prepare('DELETE FROM todo WHERE id = ?');
+    stmt.run(req.body.id, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        stmt.finalize();
+        res.redirect('/'); 
+    })
 })
 
 // Start the web server
